5b81250a8b7d6bb30c31817a0b908a8ba899d425
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-19 18:58:29 +0200

    This code is not as beautiful as those scripts other students have written and I need your constructive feedback

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 There are coins in denominations of 1, 2 UAH. and 50, 25, 10, 5 kopecks.
 
 ### Task. 
-Implement a function that returns a way to change a given amount with a minimum number of coins. If the given amount is not a multiple of 5 kopecks, you must round it to 5 in accordance with the rules of arithmetic rounding.
+Implement a function that returns a way to change a given amount with a minimum number of coins.
+If the given amount is not a multiple of 5 kopecks, you must round it to 5 in accordance
+with the rules of arithmetic rounding.
 
 To deploy project on your local machine create new virtual environment and execute this command:
 
@@ -15,6 +17,6 @@ To run all style checkers and tests use commands:
 
 `flake8 .`
 
-`pylint fishes pool config test_pool main`
+`pylint change`
 
 `mypy .`
--- change.py
+++ change.py
@@ -1,3 +1,23 @@
-def make_change(amount:str) -> dict:
+"""docstring"""
 
-    return {}
+def make_change(amount):
+    """ docstring"""
+    change_to_be_given = {}
+    new_amount = round(float(amount.strip(" UAH")), 2)
+    change_to_be_given[2] = int(new_amount // 2)
+    change_to_be_given[1] = int(new_amount - new_amount // 2 * 2)
+    kopiyky = int(new_amount % 1 * 100)
+    # arithmetic rounding is solved by check if the amount is closer
+    # to tens (even multiple of 5) or
+    # to tens plus five (odd multiples of 5)
+    if kopiyky % 5 == 0 or kopiyky // 5 % 2 == 0:
+        change_kopiyky_init = kopiyky // 5 * 5
+    else:
+        change_kopiyky_init = (kopiyky // 5 + 1) * 5
+    print(change_kopiyky_init)
+    change_to_be_given[50] = change_kopiyky_init // 50
+    change_to_be_given[25] = (change_kopiyky_init % 50) // 25
+    change_to_be_given[10] = ((change_kopiyky_init % 50) % 25) // 10
+    change_to_be_given[5] = (((change_kopiyky_init % 50) % 25) % 10) // 5
+    print(change_to_be_given)
+    return change_to_be_given
