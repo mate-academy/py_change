b90b3e6430f03ead97bcf14f90c443b57f7469b7
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-19 19:54:51 +0200

    new

--- change.py
+++ change.py
@@ -1,32 +1,33 @@
 """math"""
 from math import floor
+# 1 UAH is 100 KOP
+UAH_TO_KOP = 100
 
 
 def make_change(amount: str) -> dict:
     """docstring"""
+    output = {}
+    nominals = [200, 100, 50, 25, 10, 5]
 
     # get needed value from input
-    amount1 = amount.split()
-    amount2 = amount1[0]
-    # transform gr to kop
-    amount3 = float(amount2)
-    amount4 = floor(amount3 * 100)
+    uah = amount.split()[0]
+    # transform uah to kop
+    value_in_kop = floor(float(uah) * UAH_TO_KOP)
+
     # round to 5
-    if amount4 % 5 != 0:
-        if amount4 % 10 >= 5:
-            amount4 += (10 - amount4 % 10)
+    if value_in_kop % 5 != 0:
+        if value_in_kop % 10 >= 5:
+            value_in_kop += (10 - value_in_kop % 10)
         else:
-            amount4 -= amount4 % 10
+            value_in_kop -= value_in_kop % 10
 
-    # dict to store result
-    output = {1: 0, 2: 0, 50: 0, 25: 0, 10: 0, 5: 0}
-
-    # calculate number of coins
-    output[2] = amount4 // 200
-    output[1] = (amount4 % 200) // 100
-    output[50] = ((amount4 % 200) % 100) // 50
-    output[25] = (((amount4 % 200) % 100) % 50) // 25
-    output[10] = ((((amount4 % 200) % 100) % 50) % 25) // 10
-    output[5] = (((((amount4 % 200) % 100) % 50) % 25) % 10) // 5
+    for nominal in nominals:
+        # transform nominal according to test values
+        if nominal >= 100:
+            output[int(nominal / UAH_TO_KOP)] = value_in_kop // nominal
+            value_in_kop = value_in_kop % nominal
+        else:
+            output[nominal] = value_in_kop // nominal
+            value_in_kop = value_in_kop % nominal
 
     return output
