176746e2f4ff4a17031b913bb24e9a79f5e2c3a4
Oleh_M <morhaliuk@gmail.com>
2019-11-19 19:38:42 +0200

    Corrected

--- change.py
+++ change.py
@@ -1,24 +1,35 @@
 """Containing function make_change"""
+
+
+def gen_dict(denomination: list, num_coins: list):
+    """Generates a dict given two lists as an arguments"""
+    res = {}
+    for idx, nominal in enumerate(denomination):
+        res[nominal] = num_coins[idx]
+    return res
+
+
 def make_change(amount: str) -> dict:
     """
     Returns dict that contains change of
     given amount of money using minimum coins.
     """
-    denominations = [2, 1, 0.5, 0.25, 0.1, 0.05]
+    denomin = [200, 100, 50, 25, 10, 5]
+    num_coins = [0 for nominal in range(len(denomin))]
     money = float(amount[:amount.index(' ')])
-    change = {1: 0, 2: 0, 0.5: 0, 0.25: 0, 0.1: 0, 0.05: 0}
-    change_res = {1: 0, 2: 0, 50: 0, 25: 0, 10: 0, 5: 0}
-
-    repl = [(1, 1), (2, 2), (0.5, 50), (0.25, 25), (0.1, 10), (0.05, 5)]
 
     if str(money)[-1] != '5':
         money = round(money, 1)
+    money *= 100
 
-    for coin in denominations:
+    coin_idx = 0
+    for coin in denomin:
         while money / coin >= 1:
-            change[coin] += 1
+            num_coins[coin_idx] += 1
             money = round(money - coin, 2)
+        coin_idx += 1
 
-    for begin, end in repl:
-        change_res[end] = change[begin]
-    return change_res
+    denomin[0], denomin[1] = 1, 2
+    num_coins[0], num_coins[1] = num_coins[1], num_coins[0]
+
+    return gen_dict(denomin, num_coins)
