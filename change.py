71f22b7ca6f577e1a03bfa482a22924d36dcc29c
logotip <logotip123@yahoo.com>
2019-11-19 13:13:07 +0200

    version 1.0

--- change.py
+++ change.py
@@ -1,3 +1,25 @@
-def make_change(amount:str) -> dict:
+"""Money change counter"""
+def make_change(amount: str) -> dict:
+    """Money change counter func"""
+    result = {1: 0, 2: 0, 50: 0, 25: 0, 10: 0, 5: 0}
+    nominals = [200, 100, 50, 25, 10, 5]
 
-    return {}
+    uah = "".join(amount[:-4].split("."))
+    if int(uah) < 5:
+        return result
+    if uah[-1] == "5":
+        uah_int = int(uah)
+    elif int(uah[-1]) < 5:
+        uah_int = (int(uah[:-1]))*10
+    else:
+        uah_int = (int(uah[:-1]) + 1)*10
+
+    for i in nominals:
+        if uah_int // i:
+            if i < 100:
+                result[i] += uah_int // i
+            else:
+                result[int(i/100)] += uah_int // i
+            uah_int -= (uah_int // i) * i
+
+    return result
