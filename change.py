24650ea178baa54126bd04e43b9f5b449048a75f
ValentinaLV <tina.lysenok@gmail.com>
2019-11-20 21:07:52 +0200

    refactor method make_change with for loop

--- change.py
+++ change.py
@@ -1,32 +1,32 @@
 """
 module docstring
 """
+import math
 
 
 def make_change(amount: str) -> dict:
     """
     Returns a way to change a given amount with a minimum number of coins.
-    :param amount:
-    :return:
+    :param amount: str
+    :return: dict
     """
     money_change = {1: 0, 2: 0, 50: 0, 25: 0, 10: 0, 5: 0}
+    nominals = [200, 100, 50, 25, 10, 5]
 
     money_value = float(amount.strip(" UAH"))
-    hryvnia, kopecks = divmod(money_value, 1)
-    hryvnia = int(hryvnia)
-    kopecks = int(kopecks * 100)
-
+    kopecks = math.floor(money_value * 100)
     if kopecks % 5 != 0:
         if kopecks % 10 >= 5:
             kopecks += 10 - kopecks % 10
         else:
             kopecks -= kopecks % 10
 
-    money_change[1] = hryvnia % 2
-    money_change[2] = hryvnia // 2
-    money_change[50] = kopecks // 50
-    money_change[25] = (kopecks % 50) // 25
-    money_change[10] = ((kopecks % 50) % 25) // 10
-    money_change[5] = (((kopecks % 50) % 25) % 10) // 5
+    for nominal in nominals:
+        if nominal >= 100:
+            money_change[int(nominal / 100)] = kopecks // nominal
+            kopecks = kopecks % nominal
+        else:
+            money_change[nominal] = kopecks // nominal
+            kopecks = kopecks % nominal
 
     return money_change
