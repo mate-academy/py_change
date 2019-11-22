c3474d63ed0469ee6b76a92ac1f4e07c3f5377f2
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-22 12:31:57 +0200

    Re-written

--- change.py
+++ change.py
@@ -1,12 +1,16 @@
 """docstring"""
 
+
 def make_change(amount):
     """ docstring"""
     change_to_be_given = {}
-    new_amount = round(float(amount.strip(" UAH")), 2)
-    change_to_be_given[2] = int(new_amount // 2)
-    change_to_be_given[1] = int(new_amount - new_amount // 2 * 2)
-    kopiyky = int(new_amount % 1 * 100)
+    if "." not in amount:
+        new_amount = [amount.strip(" UAH"), "00"]
+    else:
+        new_amount = amount.strip(" UAH").split(".")
+    change_to_be_given[2] = int(new_amount[0]) // 2
+    change_to_be_given[1] = int(new_amount[0]) - int(new_amount[0]) // 2 * 2
+    kopiyky = int(new_amount[1])
     # arithmetic rounding is solved by check if the amount is closer
     # to tens (even multiple of 5) or
     # to tens plus five (odd multiples of 5)
@@ -14,10 +18,9 @@ def make_change(amount):
         change_kopiyky_init = kopiyky // 5 * 5
     else:
         change_kopiyky_init = (kopiyky // 5 + 1) * 5
-    print(change_kopiyky_init)
-    change_to_be_given[50] = change_kopiyky_init // 50
-    change_to_be_given[25] = (change_kopiyky_init % 50) // 25
-    change_to_be_given[10] = ((change_kopiyky_init % 50) % 25) // 10
-    change_to_be_given[5] = (((change_kopiyky_init % 50) % 25) % 10) // 5
-    print(change_to_be_given)
+    kop_nominals = [50, 25, 10, 5]
+    for nominal in kop_nominals:
+        change_to_be_given[nominal] = change_kopiyky_init // nominal
+        change_kopiyky_init = change_kopiyky_init % nominal
+
     return change_to_be_given
