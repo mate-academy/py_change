df359220f6a59517964bf0fb3258584f38d402ec
badian <e.mailbox.sbv@gmail.com>
2019-11-25 01:30:51 +0200

    final

--- change.py
+++ change.py
@@ -9,18 +9,13 @@ def amounter(amount):
     if isinstance(amount, str):
         flt_amount = int(float(amount.split()[0]) * 100)
         rnd_amount = flt_amount // 5 * 5
-        return int(rnd_amount)
     else:
-        return amount
+        rnd_amount = amount
+    return int(rnd_amount)
 
 def nominator(nominal):
     """uah to coin"""
-    if nominal == 1 or nominal == 2:
-        return nominal * 100
-
-    else:
-        return int(nominal)
-
+    return nominal * 100 if nominal in (1, 2) else int(nominal)
 
 def make_change(amount) -> dict:
     """make change in:str out:dict"""
@@ -37,17 +32,17 @@ def make_change(amount) -> dict:
     odd = int(clr_div * 100) - int(flr_div * 100)
     for nominal in [2, 1, 50, 25, 10, 5]:
         flr_div = amounter(amount) // nominator(nominal)
-        exit = 0
-        while exit != 1:
+        breaker = 0
+        while breaker != 1:
             if odd == 0:
                 money_dict[nominal] = flr_div
                 return money_dict
             else:
                 if flr_div == 0:
-                    exit = 1
+                    breaker = 1
                 else:
                     money_dict[nominal] = flr_div
                     odd = nominator(nominal) * flr_div
                     amount = int(amounter(amount)) - odd
-                    exit = 1
+                    breaker = 1
     return money_dict
