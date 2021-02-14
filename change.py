8d3c9710f7410e9f8a1a8d24365edea547ec1aae
Your Name <artger@ukr.net>
2021-02-14 19:33:31 +0300

    Test

--- change.py
+++ change.py
@@ -1,29 +1,23 @@
-'''Module'''
-
-
 def make_change(amount: str) -> dict:
-    '''
-    :param amount:
-    :return:
-    '''
-    answer = {1: 0, 2: 0, 50: 0, 25: 0, 10: 0, 5: 0}
-    money = amount.split(' ')[0]
-    try:
-        uah, kopecks = int(money.split('.')[0]), int(money.split('.')[1])
-    except IndexError:
-        uah, kopecks = int(money), 0
+    change =  {1: 0, 2: 0, 5: 0, 10: 0, 25: 0, 50: 0}
+    price = amount.split(' ')[0]
+    if '.' in price:
+        price = price.split(".")
+        grivna, cop = int(price[0]), int(price[1])
+        if cop % 5:
+            if cop % 10 in [1, 2, 3, 4]:
+                cop = cop - cop % 10
+            if cop % 10 in [6, 7, 8, 9]:
+                cop = cop + 10 - cop % 10
+        for i in [50, 25, 10, 5]:
+            change[i] = cop // i
+            cop = cop - change[i] * i
+    else:
+        grivna = int(price)
+    if grivna % 2:
+        change[1] = 1
+        change[2] = grivna // 2
+    else:
+        change[2] = grivna // 2
 
-    if kopecks % 5 != 0:
-        if kopecks % 10 > 5:
-            kopecks += 10 - (kopecks % 10)
-        else:
-            kopecks -= kopecks % 10
-
-    answer[2] = uah // 2
-    answer[1] = uah % 2
-    answer[50] = kopecks // 50
-    answer[25] = (kopecks % 50) // 25
-    answer[10] = ((kopecks % 50) % 25) // 10
-    answer[5] = (((kopecks % 50) % 25) % 10) // 5
-
-    return answer
+    return change
